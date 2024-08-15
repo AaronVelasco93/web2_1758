832fc4996b0ff3d5e9d45738a90486f66bd29cf2
AaronVelasco93 <aaronvelasco1993@outlook.com>
2024-08-14 18:46:59 -0600

    ejercicios de alumno

---
+++ 14082024/alumno/app.js
@@ -0,0 +1,7 @@
+alert("eEsto es una mala practica");
+
+let numero2 = 5;
+        let numero3 = 5;
+        var resultado = numero2*numero3;
+
+        console.log("El resultado de la multiplicacion " + numero2 + " y el numero " + numero3 + " es = " + resultado);
---
+++ 14082024/alumno/app2.js
@@ -0,0 +1,7 @@
+let nombre ="Emilia";
+let edad = 20;
+let nacionalidad ="mexicana";
+
+console.log("Mi nombre es " + nombre);
+console.log("Mi edad es " +edad);
+console.log("Mi nacionalidad es " +nacionalidad);
