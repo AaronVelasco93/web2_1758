0fcd81d67d289f6882534bfba0099019ed56cdee
AaronVelasco93 <aaronvelasco1993@outlook.com>
2024-11-06 18:28:30 -0600

    Conexion con db y funcionamiento

--- proyecto/app.js
+++ proyecto/app.js
@@ -9,11 +9,28 @@ app.use( bodyParser.urlencoded({extended:false}));
 //platillas que sean dinamicas
 app.set('view engine','ejs');
 
+//crear la conexion
 const db = mysql.createConnection({
     host: 'localhost',//server
-    user: 'root',
-    password: '123456',
-    database: 'node_crud',
-    port: 3306
+    user: 'root',//usuario de la DB
+    password: '123456',//pass de tu DB
+    database: 'fes_aragon3',//nombre de la base de datos
+    port: 3308//puerto
+});
+
+//comprobacion de la conexion de la base de datos
+db.connect(err=>{
+    if(err){
+        console.log(`Erro en la conexion de base de datos BB ${err}`);
+    }else {
+        console.log(`La base de datos funciona y esta conectada`)    
+    }
+});
+
+//iniciamos el server
+
+const port = 3009;
+app.listen(port,()=>{
+    console.log(`Servidor en funcionamiento desde http://localhost:${port}`);
 });
 
